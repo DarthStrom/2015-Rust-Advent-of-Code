c8c2a96f91a8ce987ec96aece62fe7a23fa637ae
Jason Duffy <jasonsduffy@gmail.com>
2020-01-15 08:55:25 -0500

    day9 cleanup

--- src/day9.rs
+++ src/day9.rs
@@ -8,9 +8,19 @@ pub fn run() {
     let lines = get_lines("day9");
 
     println!("part1: {}", shortest(&lines));
+
+    println!("part2: {}", longest(&lines));
 }
 
-fn shortest(lines: &[String]) -> usize {
+#[derive(Clone)]
+enum Method {
+    Min,
+    Max,
+}
+
+impl Copy for Method {}
+
+fn prep(lines: &[String]) -> (HashMap<(&str, &str), usize>, HashSet<&str>) {
     let mut paths: HashMap<(&str, &str), usize> = HashMap::new();
     let mut cities: HashSet<&str> = HashSet::new();
 
@@ -27,31 +37,51 @@ fn shortest(lines: &[String]) -> usize {
         paths.insert(("start", to), 0);
     }
 
-    solve(&cities, &paths, "start", HashSet::new())
+    (paths, cities)
+}
+
+fn shortest(lines: &[String]) -> usize {
+    let (paths, cities) = prep(lines);
+
+    solve(&paths, &cities, "start", HashSet::new(), Method::Min)
+}
+
+fn longest(lines: &[String]) -> usize {
+    let (paths, cities) = prep(lines);
+
+    solve(&paths, &cities, "start", HashSet::new(), Method::Max)
 }
 
 fn solve(
-    cities: &HashSet<&str>,
     paths: &HashMap<(&str, &str), usize>,
+    cities: &HashSet<&str>,
     current: &str,
     visited: HashSet<&str>,
+    method: Method,
 ) -> usize {
     if visited.len() == cities.len() {
         0
     } else {
-        let mut min = usize::MAX;
+        let mut result = match method {
+            Method::Min => usize::MAX,
+            Method::Max => 0,
+        };
         for city in cities.iter() {
             if !visited.contains(city) {
                 let mut visited = visited.clone();
                 visited.insert(*city);
-                let this =
-                    solve(&cities, &paths, city, visited) + paths.get(&(current, *city)).unwrap();
-                if this < min {
-                    min = this;
+                let this = solve(&paths, &cities, city, visited, method)
+                    + paths.get(&(current, *city)).unwrap();
+                if match method {
+                    Method::Min => this < result,
+                    Method::Max => this > result,
+                } {
+                    result = this;
                 }
             }
         }
-        min
+
+        result
     }
 }
 
@@ -60,7 +90,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn example() {
+    fn shortest_path() {
         let lines = [
             "London to Dublin = 464".to_string(),
             "London to Belfast = 518".to_string(),
@@ -68,5 +98,16 @@ mod tests {
         ];
 
         assert_eq!(shortest(&lines), 605);
+    }
+
+    #[test]
+    fn longest_path() {
+        let lines = [
+            "London to Dublin = 464".to_string(),
+            "London to Belfast = 518".to_string(),
+            "Dublin to Belfast = 141".to_string(),
+        ];
+
+        assert_eq!(longest(&lines), 982);
     }
 }
